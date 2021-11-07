3a41d701bb66ee0b3ecdc7ee9240cfeb08fd1b91
Qwinth <76917271+Qwinth@users.noreply.github.com>
2021-11-07 23:49:55 +0200

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -6,8 +6,13 @@ from datetime import datetime
 from urllib.parse import unquote
 CRLF = '\r\n'
 ctype = {'html': 'text/html', 'htm': 'text/html', 'txt': 'text/html', 'ico': 'image/x-icon', 'css': 'text/css', 'js': 'application/javascript', 'jpg': 'image/jpeg', 'png': 'image/png', 'gif': 'image/gif', 'mp3': 'audio/mp3', 'ogg': 'audio/ogg', 'wav': 'audio/wav', 'opus': 'audio/opus', 'mp4': 'video/mp4', 'webm': 'video/webm', 'other':'application/octet-stream'}
+cache = {}
+cache_num = []
+MAX_LEN = 5
 SSL = False
 def handle(sock, addr):
+    global cache
+    global cache_num
     try:
         request = sock.recv(2048)
         temp = request.decode().split('\r\n')
@@ -18,39 +23,74 @@ def handle(sock, addr):
         else:
             protocol, headers = _http.split('/')
             path = unquote(path)
-            if os.path.exists(os.getcwd() + path):
-                print(f'[{str(datetime.now()).split(".")[0]}] {addr[0]} {method} {path} 200 OK')
+            #if os.path.exists(os.getcwd() + path):
+                #print(f'[{str(datetime.now()).split(".")[0]}] {addr[0]} {method} {path} 200 OK')
 
-            elif not os.path.exists(path.split('/', 1)[1]):
-                print(f'[{str(datetime.now()).split(".")[0]}] {addr[0]} {method} {path} 404 File not found')
+            #elif not os.path.exists(path.split('/', 1)[1]):
+                #print(f'[{str(datetime.now()).split(".")[0]}] {addr[0]} {method} {path} 404 File not found')
 
-            #print(method, path, _http)
-            
             if path == '/':
                 sock.send(('HTTP/1.1 200 OK' + CRLF).encode())
                 sock.send((f'Content-Type: {ctype["html"]}' + CRLF * 2).encode())
                 sock.send(f'<meta charset="utf-8">{CRLF}'.encode())
                 if 'index.html' in os.listdir():
-                    webfile = open('index.html', 'rb')
-                    sock.send(webfile.read())
-                    webfile.close()
+                    if path in cache:
+                        sock.send(cache[path])
+                    else:
+                        webfile = open('index.html', 'rb')
+                        readfile = webfile.read()
+                        sock.send(readfile)
+                        webfile.close()
+                        if not len(cache) > MAX_LEN:
+                            cache[path] = readfile
+                            cache_num.append(path)
+                        else:
+                            del cache[cache_num[0]]
+                            cache[path] = readfile
+                            cache_num.pop(0)
     
                 elif 'index.htm' in os.listdir():
-                    webfile = open('index.htmf', 'rb')
-                    sock.send(webfile.read())
-                    webfile.close()
+                    if path in cache:
+                        sock.send(cache[path])
+                    else:
+                        webfile = open('index.htm', 'rb')
+                        readfile = webfile.read()
+                        sock.send(readfile)
+                        webfile.close()
+                        if not len(cache) > MAX_LEN:
+                            cache[path] = readfile
+                            cache_num.append(path)
+                        else:
+                            del cache[cache_num[0]]
+                            cache[path] = readfile
+                            cache_num.pop(0)
+                            cache_num.append(path)
 
                 else:
-                    sock.send(f'<h1>Directory listing for {path}</h1>{CRLF}'.encode())
-                    sock.send(f'<hr>{CRLF}'.encode())
-                    sock.send(f'<ul>{CRLF}'.encode())
-                    for i in os.listdir(os.getcwd() + path):
-                        if os.path.isdir(os.getcwd() + path + "/" + i):
-                            sock.send(f'<li><a href="{i}/">{i}</a></li>{CRLF}'.encode())
+                    #print(cache)
+                    if path in cache:
+                        sock.send(cache[path].encode())
+                        #print('ok1')
+                    else:
+                        data = f'<h1>Directory listing for {path}</h1>{CRLF}<hr>{CRLF}<ul>{CRLF}'
+                        for i in os.listdir(os.getcwd() + path):
+                            if os.path.isdir(os.getcwd() + path + "/" + i):
+                                data += f'<li><a href="{i}/">{i}</a></li>{CRLF}'
+                            else:
+                                data += f'<li><a href="{i}">{i}</a></li>{CRLF}'
+                        data += f'</ul>{CRLF}'
+                        data += f'<hr>{CRLF}'
+                        sock.send(data.encode())
+                        if len(cache) < MAX_LEN:
+                            
+                            cache[path] = data
+                            cache_num.append(path)
                         else:
-                            sock.send(f'<li><a href="{i}">{i}</a></li>{CRLF}'.encode())
-                    sock.send(f'</ul>{CRLF}'.encode())
-                    sock.send(f'<hr>{CRLF}'.encode())
+                            #print('del')
+                            del cache[cache_num[0]]
+                            cache[path] = data
+                            cache_num.pop(0)
+                            cache_num.append(path)
                     
             
             else:
@@ -58,53 +98,119 @@ def handle(sock, addr):
                         sock.send(('HTTP/1.1 200 OK' + CRLF).encode())
                         if os.path.isfile(os.getcwd() + path):
                             ext = path.split('/', 1)[1].split('.')[-1]
-                            webfile = open(path.split('/', 1)[1], 'rb')
-                            if ext in ctype:
-                                sock.send((f'Content-Type: {ctype[ext]}' + CRLF * 2).encode())
-                                sock.send(webfile.read())
-                                webfile.close()
-                        
+                            if path in cache:
+                                sock.send(cache[path])
+                                #print('ok')
                             else:
-                                sock.send((f'Content-Type: {ctype["other"]}' + CRLF * 2).encode())
-                                sock.send(webfile.read())
-                                webfile.close()
+                                #print('pathno')
+                                webfile = open(path.split('/', 1)[1], 'rb')
+                                if ext in ctype:
+                                    sock.send((f'Content-Type: {ctype[ext]}' + CRLF * 2).encode())
+                                    _type = (f'Content-Type: {ctype[ext]}' + CRLF * 2).encode()
+                                    readfile = webfile.read()
+                                    sock.send(readfile)
+                                    webfile.close()
+                                    if len(cache) < MAX_LEN:
+                                        #print('caching')
+                                        #print(path)
+                                        cache[path] = _type + readfile
+                                        cache_num.append(path)
+                                    else:
+                                        del cache[cache_num[0]]
+                                        cache[path] = _type + readfile
+                                        cache_num.pop(0)
+                                        cache_num.append(path)
+                        
+                                else:
+                                    sock.send((f'Content-Type: {ctype["other"]}' + CRLF * 2).encode())
+                                    _type = (f'Content-Type: {ctype["other"]}' + CRLF * 2).encode()
+                                    readfile = webfile.read()
+                                    sock.send(readfile)
+                                    webfile.close()
+                                    if len(cache) < MAX_LEN:
+                                        cache[path] = _type + readfile
+                                        cache_num.append(path)
+                                    else:
+                                        del cache[cache_num[0]]
+                                        cache[path] = _type + readfile
+                                        cache_num.pop(0)
+                                        cache_num.append(path)
                         else:
                             sock.send((f'Content-Type: {ctype["html"]}' + CRLF * 2).encode())
                             sock.send(f'<meta charset="utf-8">{CRLF}'.encode())
-                            if 'index.html' in os.listdir(os.getcwd() + path):
-                                webfile = open(path.split('/', 1)[1] + '/index.html', 'rb')
-                                sock.send(webfile.read())
-                                webfile.close()
-    
-                            elif 'index.htm' in os.listdir(os.getcwd() + path):
-                                webfile = open(path.split('/', 1)[1] + '/index.htm', 'rb')
-                                sock.send(webfile.read())
-                                webfile.close()
+                            #data = (f'Content-Type: {ctype["html"]}' + CRLF * 2).encode()
+                            #data += f'<meta charset="utf-8">{CRLF}'.encode()
+                            if path in cache:
+                                sock.send(cache[path])
                             else:
-                                sock.send(f'<h1>Directory listing for {path}</h1>{CRLF}'.encode())
-                                sock.send(f'<hr>{CRLF}'.encode())
-                                sock.send(f'<ul>{CRLF}'.encode())
-                                for i in os.listdir(os.getcwd() + path):
-                                    if os.path.isdir(os.getcwd() + path + "/" + i):
-                                        sock.send(f'<li><a href="{path + "/" + i}/">{i}</a></li>{CRLF}'.encode())
+                                
+                                if 'index.html' in os.listdir(os.getcwd() + path):
+                                    webfile = open(path.split('/', 1)[1] + '/index.html', 'rb')
+                                    data = webfile.read()
+                                    sock.send(data)
+                                    webfile.close()
+                                    if len(cache) < MAX_LEN:
+                                        cache[path] = data
+                                        cache_num.append(path)
                                     else:
-                                        sock.send(f'<li><a href="{path + "/" + i}">{i}</a></li>{CRLF}'.encode())
-                                sock.send(f'</ul>{CRLF}'.encode())
-                                sock.send(f'<hr>{CRLF}'.encode())
+                                        del cache[cache_num[0]]
+                                        cache[path] = data
+                                        cache_num.pop(0)
+                                        cache_num.append(path)
+    
+                                elif 'index.htm' in os.listdir(os.getcwd() + path):
+                                    data = webfile.read()
+                                    sock.send(data)
+                                    webfile.close()
+                                    if len(cache) < MAX_LEN:
+                                        cache[path] = data
+                                        cache_num.append(path)
+                                    else:
+                                        del cache[cache_num[0]]
+                                        cache[path] = data
+                                        cache_num.pop(0)
+                                        cache_num.append(path)
+                                else:
+                                    data = f'<h1>Directory listing for {path}</h1>{CRLF}'.encode()
+                                    data += f'<hr>{CRLF}'.encode()
+                                    data += f'<ul>{CRLF}'.encode()
+                                    for i in os.listdir(os.getcwd() + path):
+                                        if os.path.isdir(os.getcwd() + path + "/" + i):
+                                            data += f'<li><a href="{path + "/" + i}/">{i}</a></li>{CRLF}'.encode()
+                                        else:
+                                            data += f'<li><a href="{path + "/" + i}">{i}</a></li>{CRLF}'.encode()
+                                    data += f'</ul>{CRLF}'.encode()
+                                    data += f'<hr>{CRLF}'.encode()
+                                    sock.send(data)
+                                    if len(cache) < MAX_LEN:
+                                        cache[path] = data
+                                        cache_num.append(path)
+                                    else:
+                                        del cache[cache_num[0]]
+                                        cache[path] = data
+                                        cache_num.pop(0)
+                                        cache_num.append(path)
                     
                     else:
                         sock.send(('HTTP/1.1 404 Not Found' + CRLF).encode())
                         sock.send((f'Content-Type: {ctype["html"]}' + CRLF * 2).encode())
                         sock.send(b'<h1>404 Page not found</h>')
                 
-                
-            sock.close()
     except ConnectionResetError:
         pass
-    except:
-        sock.send(('HTTP/1.1 500 Internal Server Error' + CRLF).encode())
-        sock.send((f'Content-Type: {ctype["html"]}' + CRLF * 2).encode())
-        sock.send(b'<h1>500 Internal Server Error</h>')
+
+    except ConnectionAbortedError:
+        pass
+    
+    #except:
+        #sock.send(('HTTP/1.1 500 Internal Server Error' + CRLF).encode())
+        #sock.send((f'Content-Type: {ctype["html"]}' + CRLF * 2).encode())
+        #sock.send(b'<h1>500 Internal Server Error</h>')
+        #sock.close()
+
+    finally:
+        #print(cache_num)
+        #print('len: ', len(cache))
         sock.close()
 
 
